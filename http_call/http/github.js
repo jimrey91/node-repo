0d788dc3b83c92483f4dd7be0e328e1a70f08c4f
Jaime Reyes <jimmy_melendez@hotmail.com>
2018-01-05 15:13:16 -0600

    cambiado

--- http_call/http/github.js
+++ http_call/http/github.js
@@ -7,10 +7,10 @@
     let IE_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko';
 
     let options = {
-        host = 'api.github.com',
-        path = '/users/' + username,
-        method = 'GET',
-        headers = { 'user-agent': CHROME_USER_AGENT }
+        host: 'api.github.com',
+        path: '/users/' + username,
+        method: 'GET',
+        headers: { 'user-agent': CHROME_USER_AGENT }
     };
 
     let request = https.request(options, (response) => {
